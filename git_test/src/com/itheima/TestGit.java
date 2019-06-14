27084701defb5e95de14a3489c8e18b8c6c4a446
pjw <983241557@qq.com>
2019-06-14 17:09:16 +0800

    修改代码

--- git_test/src/com/itheima/TestGit.java
+++ git_test/src/com/itheima/TestGit.java
@@ -3,5 +3,14 @@ package com.itheima;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+    }
+    public void tse(){
+        System.out.println("oo");
+        System.out.println("oo");
+        System.out.println("oo");
     }
 }
